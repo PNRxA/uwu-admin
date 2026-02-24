d5d8ce54239558eba3204d7bb26fe69e49d333a9
PNRxA <pnrunham@gmail.com>
2026-02-25 06:47:30 +1100

    remove rate limit from protected routes

--- api/src/routes/servers.rs
+++ api/src/routes/servers.rs
@@ -5,7 +5,7 @@ use crate::handlers;
 use crate::state::SharedState;
 
 pub fn protected_routes() -> Router<SharedState> {
-    let router = Router::new()
+    Router::new()
         .route(
             "/api/auth/logout",
             post(handlers::auth::logout),
@@ -21,26 +21,5 @@ pub fn protected_routes() -> Router<SharedState> {
         .route(
             "/api/servers/{server_id}/command",
             post(handlers::servers::command),
-        );
-
-    #[cfg(not(test))]
-    let router = {
-        use std::time::Duration;
-        use axum::error_handling::HandleErrorLayer;
-        use axum::http::StatusCode;
-        use tower::ServiceBuilder;
-
-        router.layer(
-            ServiceBuilder::new()
-                .layer(HandleErrorLayer::new(
-                    |_: Box<dyn std::error::Error + Send + Sync>| async {
-                        StatusCode::TOO_MANY_REQUESTS
-                    },
-                ))
-                .buffer(100)
-                .rate_limit(30, Duration::from_secs(60)),
         )
-    };
-
-    router
 }
